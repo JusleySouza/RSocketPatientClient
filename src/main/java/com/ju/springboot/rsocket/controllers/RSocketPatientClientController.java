f166bc0e343eaef8ca0529cbc369953b7bdff9f0
Jusley Souza <jusleysouza31@gmail.com>
2022-12-08 16:25:53 -0300

    Creation and implementation of the fireAndForget route

--- src/main/java/com/ju/springboot/rsocket/controllers/RSocketPatientClientController.java
+++ src/main/java/com/ju/springboot/rsocket/controllers/RSocketPatientClientController.java
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ju.springboot.rsocket.model.ClinicalData;
@@ -27,6 +29,12 @@ public class RSocketPatientClientController {
 	public Mono<ClinicalData> requestResponse(Patient patient){
 		logger.info("Sending the rsocket request for patient: "+ patient);
 		return rSocketRequester.route("get-patient-data").data(patient).retrieveMono(ClinicalData.class);
+	}
+	
+	@PostMapping("/fire-and-forget")
+	public Mono<Void> fireAndForget( @RequestBody Patient patient){
+		logger.info("Patient Being Checked Out: "+ patient);
+		return rSocketRequester.route("patient-checkout").data(patient).retrieveMono(Void.class);
 	}
 
 }
