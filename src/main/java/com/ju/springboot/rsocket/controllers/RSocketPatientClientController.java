50ab3e48184603d156d6affa1230fe2e638bc163
Jusley Souza <jusleysouza31@gmail.com>
2022-12-08 15:25:35 -0300

    creation and implementation of the requestResponse route

--- src/main/java/com/ju/springboot/rsocket/controllers/RSocketPatientClientController.java
+++ src/main/java/com/ju/springboot/rsocket/controllers/RSocketPatientClientController.java
@@ -2,7 +2,13 @@ package com.ju.springboot.rsocket.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ju.springboot.rsocket.model.ClinicalData;
+import com.ju.springboot.rsocket.model.Patient;
+
+import reactor.core.publisher.Mono;
 
 @RestController
 public class RSocketPatientClientController {
@@ -13,6 +19,9 @@ public class RSocketPatientClientController {
 		this.rSocketRequester = builder.tcp("localhost", 7000);
 	}
 	
-	
+	@GetMapping("/request-response")
+	public Mono<ClinicalData> requestResponse(Patient patient){
+		return rSocketRequester.route("get-patient-data").data(patient).retrieveMono(ClinicalData.class);
+	}
 
 }
