cc257139c851597831a48743280df0aa30ccaea5
Jusley Souza <jusleysouza31@gmail.com>
2022-12-08 15:18:25 -0300

    Creation and implementation of the RSocketPatientClient controller

---
+++ src/main/java/com/ju/springboot/rsocket/controllers/RSocketPatientClientController.java
@@ -0,0 +1,18 @@
+package com.ju.springboot.rsocket.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RSocketPatientClientController {
+	
+	private final RSocketRequester rSocketRequester;
+	
+	public RSocketPatientClientController(@Autowired RSocketRequester.Builder builder) {
+		this.rSocketRequester = builder.tcp("localhost", 7000);
+	}
+	
+	
+
+}
